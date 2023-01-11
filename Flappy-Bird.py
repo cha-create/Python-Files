052a063459534232415edc83589709a1f406a992
Charley Griffin <boingr66@gmail.com>
2023-01-11 16:28:27 -0600

    Fixed image location error

--- Flappy-Bird.py
+++ Flappy-Bird.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pygame
 from pygame.locals import *
 
@@ -14,14 +15,11 @@ xpos = (dis_width / 2)
 ypos = (dis_width / 2)
 scrollSpeed = 4
 groundXPos = 0
-bgPath = "flappy_bird-sprites/img/bg.png"
-birdPath = "flappy_bird-sprites/img/bird1.png"
-birdFlapPath = "flappy_bird-sprites/img/bird3.png"
-groundPath = "flappy_bird-sprites/img/ground.png"
-bg = pygame.image.load(bgPath)
-bird = pygame.image.load(birdPath)
-ground = pygame.image.load(groundPath)
-birdFlap = pygame.image.load(birdFlapPath)
+bg = pygame.image.load(os.path.join("E:\\users\Boing\Documents\\Python\\random\\flappy_bird-sprites", "bg.png"))
+bird = pygame.image.load(os.path.join("E:\\users\Boing\Documents\\Python\\random\\flappy_bird-sprites", "bird1.png"))
+ground = pygame.image.load(os.path.join("E:\\Users\Boing\Documents\\Python\\random\\flappy_bird-sprites", "ground.png"))
+birdFlap = pygame.image.load(os.path.join("E:\\Users\Boing\Documents\\Python\\random\\flappy_bird-sprites", "bird3.png"))
+pipe = pygame.image.load(os.path.join("E:\\users\Boing\Documents\\Python\\random\\flappy_bird-sprites", "pipe.png"))
 birdRect = bird.get_rect()
 yDelta = 0.5
 while True:
@@ -41,6 +39,7 @@ while True:
         dis.blit(bird, (xpos, ypos))
     else:
         dis.blit(birdFlap, (xpos, ypos))
+     
     if ypos >= (bg.get_height() - bird.get_height()):
         ypos = (bg.get_height() - bird.get_height())
         yDelta = 0
